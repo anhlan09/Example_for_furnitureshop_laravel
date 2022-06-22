1c9c6e534f0df5e0ec24c42691817f74fc515847
anhlan09 <107197832+anhlan09@users.noreply.github.com>
2022-06-22 10:37:11 +0700

    Update CategoryControllerWithRepos.php

--- app/Http/Controllers/CategoryControllerWithRepos.php
+++ app/Http/Controllers/CategoryControllerWithRepos.php
@@ -10,6 +10,43 @@ use Illuminate\Support\Facades\Validator;
 class CategoryControllerWithRepos extends Controller
 {
     //
+    
+     public function index()
+        {
+            $category = CategoryRepos::getAllCategory();
+
+            return view('projects_ki_1.category.index_category',
+                [
+                    'category' => $category,
+                ]);
+        }
+
+        public function create()
+        {
+            return view('projects_ki_1.category.new_category', [
+                    "category" => (object)[
+                        'category_id' => '',
+                        'name' => '',
+                    ]
+            ]);
+
+        }
+
+        public function store(Request $request)
+        {
+            $this->formValidate($request)->validate(); //shortcut
+
+            $category = (object)[
+                'name' => $request->input('name'),
+            ];
+
+            $newId = CategoryRepos::insert($category);
+
+
+            return redirect()
+                ->action('categoryController@index')
+                ->with('msg', 'New Category with id: '.$newId.' has been inserted');
+        }
     public function edit($id){
         $category = CategoryRepos::getCategoryById($id);
 
