ed0f693b70ad246dd7abed59b43070a8883bb5dd
Huwnga <107199222+Huwnga@users.noreply.github.com>
2022-06-22 10:26:58 +0700

    Update CategoryControllerWithRepos.php

--- app/Http/Controllers/CategoryControllerWithRepos.php
+++ app/Http/Controllers/CategoryControllerWithRepos.php
@@ -10,69 +10,7 @@ use Illuminate\Support\Facades\Validator;
 class CategoryControllerWithRepos extends Controller
 {
     //
-    public function edit($id){
-        $classroom = ClassroomRepos::getClassroomById($id);
-        $teachers = TeacherRepos::getAllTeachers();
-        $selectedT = TeacherRepos::getTeachersByClassroomId($id);
-
-        return view('Classrooms.ClassroomRepos.update',
-            [
-                'classroom' => $classroom[0],
-                'teachers' => $teachers,
-                'selectedT' => $selectedT
-            ]
-        );
-    }
-
-    public function update(Request $request, $id){
-        if($id != $request->input('id')){
-            return redirect()->action('ClassroomControllerWithRepos@read');
-        }
-
-        $this->formValidate($request)->validate();
-
-        $classroom = (object)[
-            'id' => $request->input('id'),
-            'nameClassroom' => $request->input('nameClassroom'),
-            'startDate' => $request->input('startDate'),
-            'size' => $request->input('size'),
-        ];
-
-        ClassroomRepos::update($classroom);
-        $selectedT = $request->input('selectedT');
-        Classrooms_TeachersRepos::delete($classroom->id);
-        Classrooms_TeachersRepos::insert($classroom->id, $selectedT);
-
-        return redirect()->action('ClassroomControllerWithRepos@read')
-            ->with('msg', 'Update Successfully');
-    }
-
-    public function confirm($id){
-        $classroom = ClassroomRepos::getClassroomById($id);
-        $teachers = TeacherRepos::getTeachersByClassroomId($id);
-
-        return view('Classrooms.ClassroomRepos.confirm',
-            [
-                'classroom' => $classroom[0],
-                'teachers' => $teachers
-            ]
-        );
-    }
-
-    public function destroy(Request $request, $id){
-        if($id != $request->input('id')){
-            return redirect()->action('ClassroomControllerWithRepos@read');
-        }
-
-        ClassroomRepos::deleteStudentsByClassroomId($id);
-        Classrooms_TeachersRepos::delete($id);
-        ClassroomRepos::delete($id);
-
-
-        return redirect()->action('ClassroomControllerWithRepos@read')
-            ->with('msg', 'Delete Successfully');
-    }
-
+    
     private function formValidate($request){
         return Validator::make(
             $request->all(),
