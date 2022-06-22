335da32c8323ff56cffef5a5ac563157d945ead2
Huwnga <107199222+Huwnga@users.noreply.github.com>
2022-06-22 10:30:45 +0700

    Update CategoryControllerWithRepos.php

--- app/Http/Controllers/CategoryControllerWithRepos.php
+++ app/Http/Controllers/CategoryControllerWithRepos.php
@@ -10,7 +10,53 @@ use Illuminate\Support\Facades\Validator;
 class CategoryControllerWithRepos extends Controller
 {
     //
-    
+    public function edit($id){
+        $category = CategoryRepos::getCategoryById($id);
+
+        return view('Furniture_shop.CategoryRepos.update',
+            [
+                'category' => $category[0]
+            ]
+        );
+    }
+
+    public function update(Request $request, $id){
+        if($id != $request->input('category_id')){
+            return redirect()->action('CategoryControllerWithRepos@index');
+        }
+
+        $this->formValidate($request)->validate();
+
+        $category = (object)[
+            'category_id' => $request->input('category_id'),
+            'name' => $request->input('name'),
+        ];
+
+        CategoryRepos::update($category);
+
+        return redirect()->action('CategoryControllerWithRepos@index');
+    }
+
+    public function confirm($id){
+        $category = CategoryRepos::getCategoryById($id);
+
+        return view('Furniture_shop.CategoryRepos.confirm',
+            [
+                'category' => $category[0]
+            ]
+        );
+    }
+
+    public function delete(Request $request, $id){
+        if($id != $request->input('category_id')){
+            return redirect()->action('CategoryControllerWithRepos@index');
+        }
+
+        CategoryRepos::delete($id);
+
+        return redirect()->action('CategoryControllerWithRepos@index');
+    }
+
     private function formValidate($request){
         return Validator::make(
             $request->all(),
